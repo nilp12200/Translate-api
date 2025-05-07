284cbeae14ed57a55989560d637de74540557665
Nil Patel <112705642+nilp12200@users.noreply.github.com>
2025-05-07 13:16:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/translate", methods=["POST"])
+def translate():
+    data = request.json
+    res = requests.post("https://libretranslate.de/translate", json=data)
+    return jsonify(res.json())
+
+@app.route("/")
+def home():
+    return "LibreTranslate proxy is running!"
